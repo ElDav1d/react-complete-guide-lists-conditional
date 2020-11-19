308b497c6cd32da710f5d5fe53da75c41a364e22
David Vivó Palanca <david.vivo@nexum.com>
2020-11-19 10:52:17 +0100

    Create and style Char Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ValidationComponent from './ValidationComponent/ValidationComponent.js'
+import CharComponent from './CharComponent/CharComponent.js'
 
 class App extends Component {
   state = {
@@ -32,6 +33,12 @@ class App extends Component {
         </h3> 
         <ValidationComponent 
           textLength={textLength}/>
+        <ul>
+          <CharComponent char="a"/>
+          <CharComponent char="b"/>
+          <CharComponent char="c"/>
+          <CharComponent char="d"/>
+        </ul>
       </div>
     );
   }
---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const style = {
+  display: 'inline-block',
+  padding: '1rem',
+  textAlign: 'center',
+  margin: '1rem',
+  border: '1px solid black'
+}
+
+const charComponent = (props) => {
+  return (
+  <li style={style}>{props.char}</li>
+  )
+};
+
+export default charComponent;
